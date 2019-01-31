734c91f5c8b96e3508ae4409bede4d27e8ff4976
cswang <cswangforwork@outlook.com>
2019-01-31 11:47:52 +0800

    teacher comment for student

--- ChuXinEdu.CMS.WEB/src/page/student/regular/index.vue
+++ ChuXinEdu.CMS.WEB/src/page/student/regular/index.vue
@@ -24,10 +24,17 @@
                 v-bind:studentCode="studentCode">
             </student-achievements>
         </el-tab-pane>
+
         <el-tab-pane :name="'feeback'">
             <span slot="label"><i class="fa fa-exclamation-triangle"></i> 退 费</span> 
             <student-fee-back v-if="activeLabelCode === 'feeback'" v-bind:studentCode="studentCode"></student-fee-back>
         </el-tab-pane>
+
+        <el-tab-pane :name="'comment'">
+            <span slot="label"><i class="fa fa-flag-checkered"></i> 课堂评语</span> 
+            <teacher-comment v-if="activeLabelCode === 'comment'" v-bind:studentCode="studentCode"></teacher-comment>
+        </el-tab-pane>
+
         <el-tab-pane :name="'otherinfo'">
             <span slot="label"><i class="fa fa-handshake-o"></i> 其他信息</span> 
             <student-additional-info v-if="activeLabelCode === 'otherinfo'" v-bind:studentCode="studentCode"></student-additional-info>
@@ -43,6 +50,7 @@ import courseHistory from './courseHistory'
 import dayoffHistory from './dayoffHistory'
 import achievements from './achievements'
 import feeBack from './feeBack'
+import comment from './teacherComment'
 import additionalInfo from './additionalInfo'
 
 export default {
@@ -61,6 +69,7 @@ export default {
         "student-offday-history" : dayoffHistory,
         "student-achievements": achievements,
         "student-fee-back": feeBack,
+        "teacher-comment": comment,
         "student-additional-info":additionalInfo
     },
     methods: {
